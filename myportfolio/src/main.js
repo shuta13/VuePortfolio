3276bf461d6ac410a856714729f45fdcad60647e
Hirai Shuta <is0471ve@ed.ritsumei.ac.jp>
2018-12-20 00:59:06 +0900

    updated

--- myportfolio/src/main.js
+++ myportfolio/src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import VirtualCollection from 'vue-virtual-collection'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+
 import './assets/sass/style.scss'
 
 Vue.config.productionTip = false
 Vue.config.devtools = true
 
 Vue.use(VirtualCollection)
+Vue.use(VueRouter)
 
 new Vue({
   render: h => h(App),
